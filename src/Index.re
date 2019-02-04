a4c0507b7d02b8440cebef0fa8fbeca0fd976b80
Mathieu Acthernoene <zoontek@gmail.com>
2019-02-04 23:08:44 +0100

    Add sane css defaults

--- src/Index.re
+++ src/Index.re
@@ -1,1 +1,40 @@
+[%bs.raw {|require('normalize.css')|}];
+[%bs.raw {|require('./assets/css/reset.css')|}];
+
+[@bs.module]
+external quasimodaRegular: string = "./assets/fonts/quasimoda_regular.woff";
+[@bs.module]
+external quasimodaBold: string = "./assets/fonts/quasimoda_bold.woff";
+
+let fonts = [|
+  "Quasimoda",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "\"Segoe UI\"",
+  "Helvetica",
+  "Arial",
+  "sans-serif",
+  "\"Apple Color Emoji\"",
+  "\"Segoe UI Emoji\"",
+  "\"Segoe UI Symbol\"",
+|];
+
+Css.(
+  fontFace(
+    ~fontFamily="Quasimoda",
+    ~src=[url(quasimodaRegular)],
+    ~fontStyle=`normal,
+    ~fontWeight=`num(400),
+    (),
+  ),
+  fontFace(
+    ~fontFamily="Quasimoda",
+    ~src=[url(quasimodaBold)],
+    ~fontStyle=`normal,
+    ~fontWeight=`num(700),
+    (),
+  ),
+  global("body", [fontFamily(Js.Array.join(fonts))]),
+);
+
 ReactDOMRe.renderToElementWithId(<App />, "root");
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,68 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const outputDir = path.join(__dirname, "build/");
-const isProd = process.env.NODE_ENV === "production";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleExtHtmlWebpackPlugin = require("style-ext-html-webpack-plugin");
+
+const outputDir = path.join(__dirname, "build");
+const isProduction = process.env.NODE_ENV === "production";
+
+const getAssetName = parentDir =>
+  process.env.NODE_ENV === "development"
+    ? "[path][name].[ext]"
+    : path.join("assets", parentDir, "[name].[hash].[ext]");
 
 module.exports = {
-  entry: "./src/Index.bs.js",
-  mode: isProd ? "production" : "development",
+  entry: path.join(__dirname, "src", "Index.bs.js"),
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
   output: {
     path: outputDir,
-    filename: "Index.js",
+    sourceMapFilename: "bundle.[hash].js.map",
+    filename: "bundle.[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: getAssetName("images"),
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: getAssetName("fonts"),
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      inject: false,
+      template: path.join("src", "index.html"),
+      inject: true,
+      minify: isProduction && {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin(),
+    new StyleExtHtmlWebpackPlugin({
+      minify: isProduction,
     }),
   ],
   devServer: {
